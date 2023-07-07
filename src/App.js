d38c8dffe79c991757a4098aee83e372ce196c4e
Sandro Ptak <sandroptak@gmail.com>
2023-07-07 12:40:27 -0300

    Feat: add nav bar and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/acessar">Login</Link>
+        <Link to="/criar-post">Criar Post</Link>
+      </nav>
+      <Routes>
+        <Route path="/pagina-inicial" element={<Home/>}/>
+        <Route path="/acessar" element={<Login/>}/>
+        <Route path="/criar-post" element={<CreatePost/>}/>
+        <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
+      </Routes>
+    </Router>
   );
 }
 
